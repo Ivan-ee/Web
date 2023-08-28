7fa87401da4b3957c1ab7bb9141dc44e225aea99
Smirnov Ivan <smirnowwwivan@mail.ru>
2023-08-28 11:13:33 +0300

    Project - 16

---
+++ react-projects/practice/16/src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <>
+            <h1>Ivan</h1>
+            <h2>I'm teach React!!!</h2>
+            <button>Kile!</button>
+        </>
+    )
+}
+
+export default Card
